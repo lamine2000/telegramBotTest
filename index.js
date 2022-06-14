1d1db86db68d974e47dd52be6ca2e6db996b8755
lamine2000 <lemzogueye.lg72@gmail.com>
2022-06-14 11:08:57 +0000

    fist commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const secrets = require('./secrets.js') 
+const { Telegraf } = require('telegraf')
+const fs = require('fs')
+
+const bot = new Telegraf(`${secrets.token}`)
+
+const inlineReply = (ctx, answer) => {
+    ctx.telegram.sendMessage(
+        ctx.message.chat.id,
+        `${answer}`, 
+        {reply_to_message_id: ctx.message.message_id})
+}
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.on('voice', ctx => ctx.reply('ecris lawax'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('video', ctx => ctx.replyWithVideo({source: 'video.mp4'}))
+
+//commands
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('hipster', ctx => {
+    inlineReply(ctx, "I'm not a hipster!")
+})
+
+//launching bot
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
