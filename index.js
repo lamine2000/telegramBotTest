fbfca5ca8ccb90a0b9574be7d12447f1ee89d37a
lamine2000 <lemzogueye.lg72@gmail.com>
2022-07-05 13:34:16 +0000

    Test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const secrets = require('./secrets.js') 
 const { Telegraf } = require('telegraf')
-const fs = require('fs')
+require('fs')
 
 const bot = new Telegraf(`${secrets.token}`, {
   telegram: {
@@ -21,6 +21,7 @@ bot.hears('hi', ctx => ctx.reply('Hey there'))
 bot.on('new_chat_members', ctx => ctx.reply('Hello the new one'))
 bot.on('voice', ctx => ctx.reply('ecris lawax'))
 bot.on('sticker', ctx => ctx.reply('👍'))
+bot.hears('gallo', ctx => inlineReply(ctx, 'Phrase'))
 
 //commands
 bot.command('video', ctx => ctx.replyWithVideo({source: 'video.mp4'}))
